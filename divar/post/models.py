6ddd2a10edb5636b66ebffd89fd6aed4119c5179
aria <aria.afsar@gmail.com>
2024-11-09 18:50:38 +0330

    added post models

--- divar/post/models.py
+++ divar/post/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from user.models import Account
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(Account, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    create_time = models.DateTimeField(auto_now_add=True)
 
 # Create your models here.
