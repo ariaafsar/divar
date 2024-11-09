e187bf0a0667a4127757183c825094c36f5e18a3
aria <aria.afsar@gmail.com>
2024-11-09 18:50:48 +0330

    added user models

--- divar/user/models.py
+++ divar/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from post.models import Post
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=11)
+    email = models.EmailField(max_length=255)
+    published_posts = models.ForeignKey(Post , on_delete=models.CASCADE)
 
 # Create your models here.
